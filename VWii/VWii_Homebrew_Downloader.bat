84305f4b34f12f128e8aa187e2b3648a474f255c
Mattamech 3.0 <43054657+Mattamech@users.noreply.github.com>
2020-04-17 07:53:55 -0600

    Update VWii_Homebrew_Downloader.bat

--- VWii/VWii_Homebrew_Downloader.bat
+++ VWii/VWii_Homebrew_Downloader.bat
@@ -6,9 +6,7 @@ mkdir sd
 pause
 cd C:\VWii_Homebrew\sd
 curl https://wii.guide/assets/files/homebrew_browser_v0.3.9e.zip --output homebrew_browser.zip
-curl https://github.com/Mattamech/Wii-or-VWii-Homebrew-Installer/raw/master/hackmii_installer_v1.2.zip --output hackmii_installer.zip
 curl http://stahlworks.com/dev/unzip.exe --output unzip.exe
-curl http://wiiubru.com/appstore/zips/wuphax.zip --output wuphax.zip
 pause
 unzip *zip
 pause
