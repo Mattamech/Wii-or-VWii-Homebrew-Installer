616aa13899eaef5877a73ad55d6441bde1fa0c18
Mattamech 3.0 <43054657+Mattamech@users.noreply.github.com>
2020-04-16 18:54:14 -0600

    Create Wii_or_VWii_Homebrew_Downloader.bat

---
+++ Wii_or_VWii_Homebrew_Downloader.bat
@@ -0,0 +1,13 @@
+C:
+cd C:
+mkdir Wii_Homebrew
+cd C:\Wii_Homebrew
+mkdir sd
+pause
+cd C:\Wii_Homebrew\sd
+curl https://wii.guide/assets/files/homebrew_browser_v0.3.9e.zip --output homebrew_browser.zip
+curl https://github.com/Mattamech/Wii-or-VWii-Homebrew-Installer/raw/master/hackmii_installer_v1.2.zip --output hackmii_installer.zip
+curl http://stahlworks.com/dev/unzip.exe --output unzip.exe
+pause
+unzip *zip
+pause
