6a7490b6a98ec1c7a4a01de44f699e684ce95465
Mattamech 3.0 <43054657+Mattamech@users.noreply.github.com>
2020-04-17 06:26:25 -0600

    Create Homebrew_Installer_BASE.bat
    This file serves as the second part of a minimalist base installation.

---
+++ Homebrew_Installer_BASE.bat
@@ -0,0 +1,8 @@
+((NOTE: Due to how the destination chould be very different par download, this file must be edited. All you have to do is edit the text in the other parenthesis and delete this line and any other parenthesis.))
+cd C:\Wii_homebrew\sd
+del /f *.bat
+Xcopy /E /I C:\Wii_homebrew\sd (Place location of the ROOT of your sd card here)
+pause
+cd C:\Wii_homebrew
+del /f *bat
+pause
