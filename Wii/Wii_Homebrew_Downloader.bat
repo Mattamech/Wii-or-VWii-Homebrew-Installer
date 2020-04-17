6d70bcd1b2e83a4f82fcc05aeb69175a151ff7c6
Mattamech 3.0 <43054657+Mattamech@users.noreply.github.com>
2020-04-16 19:32:28 -0600

    Create Wii_Homebrew_Downloader.bat

---
+++ Wii/Wii_Homebrew_Downloader.bat
@@ -0,0 +1,16 @@
+C:
+cd C:
+mkdir Wii_Homebrew
+cd C:\Wii_Homebrew
+mkdir sd
+pause
+cd C:\Wii_Homebrew\sd
+curl https://wii.guide/assets/files/homebrew_browser_v0.3.9e.zip --output homebrew_browser.zip
+curl https://github.com/Mattamech/Wii-or-VWii-Homebrew-Installer/raw/master/hackmii_installer_v1.2.zip --output hackmii_installer.zip
+curl http://stahlworks.com/dev/unzip.exe --output unzip.exe
+curl https://github.com/emukidid/cleanrip/releases/download/2.1.1/CleanRip-v2.1.1.zip --output CleanRip.zip
+curl https://wii.guide/assets/files/Priiloader_v0_8_2.zip --output Priiloader.zip
+pause
+unzip *zip
+pause
+exit
