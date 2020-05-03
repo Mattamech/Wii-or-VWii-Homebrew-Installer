17f50229bc30fa01661b4a9401ee6b8c23fe8f55
Mattamech 3.0 <mattamech3.0@gmail.com>
2020-05-03 16:31:18 -0600

    Fixed issuse 2

--- Version 2/Homebrew_Downloader_BASE.bat
+++ Version 2/Homebrew_Downloader_BASE.bat
@@ -6,6 +6,7 @@ mkdir sd
 pause
 cd C:\Wii_Homebrew\sd
 curl https://wii.guide/assets/files/homebrew_browser_v0.3.9e.zip --output homebrew_browser.zip
+curl https://mattamech.github.io/Wii-or-VWii-Homebrew-Installer/docs/hackmii_installer_v1.2.zip -o hackmii_installer_v1.2.zip
 curl http://stahlworks.com/dev/unzip.exe --output unzip.exe
 pause
 unzip *zip
--- Version 2/VWii/VWii_Homebrew_Downloader.bat
+++ Version 2/VWii/VWii_Homebrew_Downloader.bat
@@ -6,6 +6,8 @@ mkdir sd
 pause
 cd C:\VWii_Homebrew\sd
 curl https://wii.guide/assets/files/homebrew_browser_v0.3.9e.zip --output homebrew_browser.zip
+curl https://mattamech.github.io/Wii-or-VWii-Homebrew-Installer/docs/hackmii_installer_v1.2.zip -o hackmii_installer_v1.2.zip
+curl https://mattamech.github.io/Wii-or-VWii-Homebrew-Installer/docs/wuphax.zip -o wuphax.zip
 curl http://stahlworks.com/dev/unzip.exe --output unzip.exe
 pause
 unzip *zip
