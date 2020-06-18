f0a2dfd786ad3e4f4288159e947862d37d47dcfe
Mattamech 3.0 <mattamech3.0@gmail.com>
2020-06-18 07:51:56 -0600

    Create downloader.bat

---
+++ docs/downloader.bat
@@ -0,0 +1,4 @@
+curl https://wii.guide/assets/files/homebrew_browser_v0.3.9e.zip --output homebrew_browser.zip
+curl https://wii.guide/assets/files/Priiloader_v0_8_2.zip --output Priiloader.zip
+curl https://mattamech.github.io/Wii-or-VWii-Homebrew-Installer/docs/hackmii_installer_v1.2.zip -o hackmii_installer_v1.2.zip
+curl http://stahlworks.com/dev/unzip.exe --output unzip.exe
