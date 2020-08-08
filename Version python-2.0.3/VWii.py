4dc8bedff12737e3691064f222202a09ee53ec26
Mattamech 3.0 <mattamech3.0@gmail.com>
2020-08-08 10:47:58 -0600

    Update VWii.py

--- Version python-2.0.3/VWii.py
+++ Version python-2.0.3/VWii.py
@@ -1,5 +1,3 @@
-import wget
-from shutil import copyfile
 import os
 import shutil
 
@@ -8,10 +6,12 @@ os.chdir(Download_Location)
 print(os.getcwd())
 input("If the folder above is not correct you will have to restart. Press enter to continue.")
 
-wget.download('https://mattamech.github.io/Wii-or-VWii-Homebrew-Installer/docs/vwii_downloader.bat', 'vwii_downloader.bat')
-
-clear = lambda: os.system('cls')
-clear()
+vwii_downloader = open('vwii_downloader.bat','w')
+vwii_downloader.write('curl https://wii.guide/assets/files/homebrew_browser_v0.3.9e.zip --output homebrew_browser.zip\n')
+vwii_downloader.write('curl https://mattamech.github.io/Wii-or-VWii-Homebrew-Installer/docs/hackmii_installer_v1.2.1.zip -o hackmii_installer_v1.2.zip\n')
+vwii_downloader.write('curl https://mattamech.github.io/Wii-or-VWii-Homebrew-Installer/docs/wuphax.zip -o wuphax.zip\n')
+vwii_downloader.write('curl http://stahlworks.com/dev/unzip.exe --output unzip.exe')
+vwii_downloader.close()
 
 input("Double Check that vwii_downloader.bat is now in the folder you specifyed. Press enter to continue.")
 
@@ -21,7 +21,14 @@ os.system('cmd /c del /f *.zip')
 os.system('cmd /c del /f *.txt')
 os.system('cmd /c del /f *.exe')
 os.system('cmd /c del /f *.bat')
-wget.download('https://mattamech.github.io/Wii-or-VWii-Homebrew-Installer/docs/vwii_copier.bat', 'vwii_copier.bat')
+vwii_copier = open('vwii_copier.bat','w')
+vwii_copier.write('mkdir apps\n')
+vwii_copier.write('cd apps\n')
+vwii_copier.write('mkdir homebrew_browser\n')
+vwii_copier.write('cd ../\n')
+vwii_copier.write('Xcopy /E /I homebrew_browser apps\homebrew_browser\n')
+vwii_copier.write('RD /S /Q homebrew_browser')
+vwii_copier.close()
 os.system('cmd /c vwii_copier.bat')
 os.system('cmd /c del /f *.bat')
 os.system('cmd /c del /f *.install')
@@ -35,11 +42,15 @@ shutil.copytree('apps', 'copy_to_sd/apps')
 shutil.copytree('wiiu', 'copy_to_sd/wiiu')
 
 Save_Location = input("Enter Drive letter (ex G:) here:")
-shutil.move('copy_to_sd/boot.elf', Save_Location)
-shutil.move('copy_to_sd/bootmini.elf', Save_Location)
-shutil.move('copy_to_sd/apps', Save_Location)
-shutil.move('copy_to_sd/wiiu', Save_Location)
-
+vwii_final = open('vwii_final.bat','w')
+vwii_final.write('Xcopy /E /I copy_to_sd ')
+vwii_final.write(Save_Location)
+vwii_final.close()
+os.system('cmd /c vwii_final.bat')
+os.system('cmd /c RD /S /Q apps')
+os.system('cmd /c RD /S /Q wiiu')
+os.system('cmd /c del /f *.elf')
+os.system('cmd /c del /f *.bat')
 
 input("If you followed the directions correctly the files should be in the root of the sd card. Press enter to exit.")
 exit()
