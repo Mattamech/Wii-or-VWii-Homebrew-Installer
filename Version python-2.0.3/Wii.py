c81a40051be9ee58be4357ed5f3fbd3ba9407e5c
Mattamech 3.0 <mattamech3.0@gmail.com>
2020-08-08 10:00:34 -0600

    Update Wii.py

--- Version python-2.0.3/Wii.py
+++ Version python-2.0.3/Wii.py
@@ -1,4 +1,3 @@
-import wget
 import os
 import shutil
 
@@ -40,11 +39,12 @@ shutil.copytree('apps', 'copy_to_sd/apps')
 shutil.copytree('extra', 'copy_to_sd/extra')
 
 Save_Location = input("Enter drive letter (EX: G:) here:")
-shutil.move('copy_to_sd/boot.elf', Save_Location)
-shutil.move('copy_to_sd/bootmini.elf', Save_Location)
-shutil.move('copy_to_sd/apps', Save_Location)
-shutil.move('copy_to_sd/extra', Save_Location)
-
+wii_final = open('wii_final.bat','w')
+wii_final.write('Xcopy /E /I copy_to_sd ')
+wii_final.write(Save_Location)
+wii_final.close()
+os.system('cmd /c wii_final.bat')
+os.system('cmd /c del /f *.bat')
 
 input("If you followed the directions correctly the files should be in the root of the sd card. Press enter to exit.")
 exit()
