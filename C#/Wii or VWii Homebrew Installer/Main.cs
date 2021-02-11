14864f3772a57d9bd09995976028b0702da427a2
Lord-Giganticus <mrubins64@gmail.com>
2021-02-11 12:50:28 -0600

    Finish up code

--- C#/Wii or VWii Homebrew Installer/Main.cs
+++ C#/Wii or VWii Homebrew Installer/Main.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
 
 namespace Wii_or_VWii_Homebrew_Installer
 {
@@ -6,6 +9,16 @@ namespace Wii_or_VWii_Homebrew_Installer
     {
         static void Main()
         {
+            if (Directory.Exists("Copy_to_SD"))
+            {
+                Console.WriteLine("Copy_to_SD folder dectected. Jumping to Copier code.");
+                Thread.Sleep(2000);
+                goto Copier;
+            }
+            else
+            {
+                //pass
+            }
             Console.WriteLine("Chose which Wii Variant you want to ready your sd card for:\n[1]Wii\n[2]VWii\n");
             string Choice = Console.ReadLine();
             if (Choice == "1")
@@ -13,7 +26,23 @@ namespace Wii_or_VWii_Homebrew_Installer
                 Wii.Download();
                 Wii.Extract();
                 Wii.Move();
+                goto Copier;
+            } else if (Choice == "2")
+            {
+                VWii.Download();
+                VWii.Extract();
+                VWii.Move();
+                goto Copier;
             }
+        Copier:
+        Environment.CurrentDirectory = Directory.GetCurrentDirectory();
+        Console.WriteLine("Enter the drive you want to copy the files to:");
+        string drive = Console.ReadLine();
+        Process process = Process.Start("CMD.exe", "/c robocopy /E Copy_to_SD \"" + drive + "\"");
+        process.WaitForExit();
+        Console.WriteLine("Complete. Exiting.");
+        Thread.Sleep(5000);
+        Environment.Exit(0);
         }
     }
 }
--- C#/Wii or VWii Homebrew Installer/VWii.cs
+++ C#/Wii or VWii Homebrew Installer/VWii.cs
@@ -3,19 +3,17 @@ using System.Diagnostics;
 using System.IO;
 using System;
 
-
 namespace Wii_or_VWii_Homebrew_Installer
 {
-    class Wii
+    class VWii
     {
         public static void Download()
         {
-            
             using (var client = new WebClient())
             {
                 client.DownloadFile("https://wii.guide/assets/files/homebrew_browser_v0.3.9e.zip", "homebrew_browser.zip");
-                client.DownloadFile("https://wii.guide/assets/files/Priiloader_v0_9.zip", "Priiloader.zip");
-                client.DownloadFile("https://mattamech.github.io/Wii-or-VWii-Homebrew-Installer/hackmii_installer_v1.2.1.zip", "hackmii_installer_v1.2.zip");
+                client.DownloadFile("https://mattamech.github.io/Wii-or-VWii-Homebrew-Installer/docs/hackmii_installer_v1.2.1.zip", "hackmii_installer_v1.2.zip");
+                client.DownloadFile("https://www.wiiubru.com/appstore/zips/wuphax.zip", "wuphax.zip");
                 client.DownloadFile("http://stahlworks.com/dev/unzip.exe", "unzip.exe");
             }
             return;
@@ -23,13 +21,13 @@ namespace Wii_or_VWii_Homebrew_Installer
         public static void Extract()
         {
             Environment.CurrentDirectory = Directory.GetCurrentDirectory();
-            Process.Start("CMD.exe", "/c unzip *.zip & del /f *.zip & del /f *.txt & del /f unzip.exe & exit").WaitForExit();
+            Process.Start("CMD.exe", "/c unzip *.zip & del /f *.zip & del /f *.txt & del /f unzip.exe & exit");
             return;
         }
         public static void Move()
         {
             Environment.CurrentDirectory = Directory.GetCurrentDirectory();
-            Process.Start("CMD.exe", "/c mkdir Copy_to_SD & robocopy /E homebrew_browser apps & RD /S /Q homebrew_browser & RD /S /Q \"Homebrew Browser Guide and Help\" & move boot.elf Copy_to_SD & move bootmini.elf Copy_to_SD & move apps Copy_to_SD & move extra Copy_to_SD & exit").WaitForExit();
+            Process.Start("CMD.exe", "/c mkdir apps & move homebrew_browser apps & RD /S /Q \"Homebrew Browser Guide and Help\" & move boot.elf Copy_to_SD & move bootmini.elf Copy_to_SD & move apps Copy_to_SD & move wiiu Copy_to_SD & exit").WaitForExit();
         }
     }
 }
